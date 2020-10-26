8f55c2d7ea1b6ba20480e8c936627631e1790b3d
Hombre7Electrico <73086720+Hombre7Electrico@users.noreply.github.com>
2020-10-26 20:19:24 -0300

    Integré lo de nadia

---
+++ PROYECTO/Unit9_Estado_carbonatacion.~dfm
@@ -0,0 +1,25 @@
+object Form9: TForm9
+  Left = 192
+  Top = 114
+  Width = 928
+  Height = 414
+  Caption = 'Estado carbonataci'#243'n'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 96
+    Top = 80
+    Width = 75
+    Height = 25
+    Caption = 'Cerrar'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+end
---
+++ PROYECTO/unit1.cpp
@@ -0,0 +1,49 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+//---------------------------------------------------------------------------
+USEFORM("project.cpp", Form1);
+USEFORM("Unit2.cpp", Form2);
+USEFORM("Unit3.cpp", Form3);
+USEFORM("Unit4.cpp", Form4);
+USEFORM("Unit5.cpp", Form5);
+USEFORM("Unit6_Estado_Agua.cpp", Form6);
+USEFORM("Unit7_Estado_jsimple.cpp", Form7);
+USEFORM("Unit8_Estado_jconcentrado.cpp", Form8);
+USEFORM("Unit9_Estado_carbonatacion.cpp", Form9);
+//---------------------------------------------------------------------------
+WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+        try
+        {
+                 Application->Initialize();
+                 Application->CreateForm(__classid(TForm1), &Form1);
+                 Application->CreateForm(__classid(TForm2), &Form2);
+                 Application->CreateForm(__classid(TForm3), &Form3);
+                 Application->CreateForm(__classid(TForm4), &Form4);
+                 Application->CreateForm(__classid(TForm5), &Form5);
+                 Application->CreateForm(__classid(TForm6), &Form6);
+                 Application->CreateForm(__classid(TForm7), &Form7);
+                 Application->CreateForm(__classid(TForm8), &Form8);
+                 Application->CreateForm(__classid(TForm9), &Form9);
+                 Application->Run();
+        }
+        catch (Exception &exception)
+        {
+                 Application->ShowException(&exception);
+        }
+        catch (...)
+        {
+                 try
+                 {
+                         throw Exception("");
+                 }
+                 catch (Exception &exception)
+                 {
+                         Application->ShowException(&exception);
+                 }
+        }
+        return 0;
+}
+//---------------------------------------------------------------------------
